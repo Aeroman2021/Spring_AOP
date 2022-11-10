b5fc1b3650114899f20cada86b3b656ea74f8b44
malakouti.aero@gmail.com <malakouti.aero@gmail.com>
2022-11-10 17:24:13 +0330

    no message

---
+++ src/main/java/ir/tamin/aophomework/main/exceptions/AccountException.java
@@ -0,0 +1,21 @@
+package ir.tamin.aophomework.main.exceptions;
+
+public class AccountException extends RuntimeException{
+    public AccountException() {
+        super();
+    }
+
+    public AccountException(String message) {
+        super(message);
+    }
+
+    public AccountException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccountException(Throwable cause) {
+        super(cause);
+    }
+
+
+}
