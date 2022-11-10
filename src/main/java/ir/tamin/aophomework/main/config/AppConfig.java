ebdacd1190a150ed461e801997175dec2c5543b6
malakouti.aero@gmail.com <malakouti.aero@gmail.com>
2022-11-10 17:19:36 +0330

    no message

---
+++ src/main/java/ir/tamin/aophomework/main/config/AppConfig.java
@@ -0,0 +1,13 @@
+package ir.tamin.aophomework.main.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("ir.tamin.aophomework.main")
+public class AppConfig {
+
+}
